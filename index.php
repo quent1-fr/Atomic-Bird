75e20faef8c5c6ffc988eea7540ed78a8ebddd1f
Lancelot HARDEL <debetux@gmail.com>
2013-02-21 22:22:02 +0100

    Passage aux regex, et ça revient à de l'atom.

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-    include 'include/simplehtmldom.inc';
     include 'include/fonctions.inc';
     include 'include/config.inc';
     date_default_timezone_set('UTC');
@@ -10,59 +9,76 @@
     
     $username = strtolower($_GET['username']);
     
-    if(cache_age($username) > $cache_time * 60){
+    if(1==1){
         ini_set('user_agent', $user_agent);
         
-        $mobile_timeline = @file_get_contents('https://mobile.twitter.com/' . $username);
+        // $mobile_timeline = @file_get_contents('https://mobile.twitter.com/' . $username);
+        // write_cache('debetux', $mobile_timeline);
+        $mobile_timeline = read_cache('debetux');
     
         if(!$mobile_timeline) die('<h1>Erreur</h1><p>Le twittos n\'existe pas!</p>');
-        
-        $timeline = new simple_html_dom();
-        $timeline->load($mobile_timeline);
-    
-        $feed = array(
-            'name'        => trim($timeline->find('div.fullname', 0)->innertext),
-            'url'         => $timeline->find('div.url', 0)->innertext,
-            'description' => $timeline->find('div.bio', 0)->innertext . '<br />' . $timeline->find('div.url', 0)->innertext,
-            'date'        => $timeline->find('td.timestamp'),
-            'timeline'    => $timeline->find('div.tweet-text')
-        );
-        
-        $rss = '<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://www.w3.org/2005/Atom">
-        <title>Timeline de ' . $feed['name']. ' (@' . $username . ')</title><subtitle><![CDATA[' . htmlentities($feed['description']) . ']]</subtitle>
-        <updated>' . date('c', strtotimestamp($feed['date'][0]->plaintext)) . '</updated><link href="https://twitter.com/' . $username . '" />
-        <author><name>' . $feed['name'] . '</name><uri>' . $feed['url'] . '</uri></author>';
 
+        # On y va a grand coup de regex pour récupérer tout qui va bien.
+        # Le nom déjà
+        // $regex_fullname = '#<div class="fullname#';
+        // $regex_fullname = '/<div class="(.*)/iU';
+        $regex_fullname = '#<div class="fullname">(.*)#';
+        preg_match($regex_fullname, $mobile_timeline, $fullname);
+        $info['fullname'] = $fullname[1];
+        $info['username'] = $_GET['username'];
+
+        # La bio aussi
+        $regex_bio = '#<div class="bio"><div class="dir-ltr" dir="ltr">(.*)</div></div>#';
+        preg_match($regex_bio, $mobile_timeline, $bio);
+        $info['bio'] = $bio[1];
+
+        # Passons aux tweets !
+        // TODO : différencier les RT des tweets.
+        $regex_tweets = '#<div class="tweet-text" data-id="[0-9]*"><div class="dir-ltr" dir="ltr">(.*)</div></div>#iU';
+        preg_match_all($regex_tweets, $mobile_timeline, $tweets);
+        echo "<pre>";
+        // print_r($tweets[1]);
+        $tweets = $tweets[1];
+
+        # URLs des tweets
+        $regex_tweets_url = '#<table class="tweet" href="(.*)">#iU';
+        preg_match_all($regex_tweets_url, $mobile_timeline, $tweets_url);
+        // print_r($tweets_url[1]);
+        $tweets_url = $tweets_url[1];
+
+        # La date !
+        $regex_tweets_date = '#<a name="tweet_[0-9]*" href=".*">(.*)</a>#iU';
+        preg_match_all($regex_tweets_date, $mobile_timeline, $tweets_date);
+        // print_r($tweets_date[1]);
+        $tweets_date = $tweets_date[1];
+
+        // foreach ($tweets_url[1] as $key => $value) {
+        //     echo '<a href="https://twitter.com'.$value.'">'.$key.'</a><br/>';
+        // }
+
+        // print_r($info);
+
+        # 
+        // foreach ($r as $v) {
+        //     echo htmlspecialchars($v);
+        // }
+        //echo preg_match($regex_fullname, $mobile_timeline);
+
+        // echo $mobile_timeline;
         $rss = 
-        '<?xml version="1.0" encoding="UTF-8"?> <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:georss="http://www.georss.org/georss" version="2.0">
-            <channel>
-                <title>Twitter / '.$username.'</title>
-                <link>http://twitter.com/'.$username.'</link>
-                <description>Twitter updates from '.$username.'.</description>
-                <language>en-us</language>
-                <ttl>40</ttl>';
-
-        $nbrdate = count($feed['date']) - 1;
-        for($i = 0; $i < $nbrdate; $i++)
-            if((!$show_mentions && $feed['timeline'][$i]->plaintext[0] != '@') or $show_mentions)
-                $rss .= '
-                <item>
-                    <title>'.$username.': '. htmlspecialchars(utf8_encode(htmlentities(rtrim(substr($feed['timeline'][$i]->plaintext, 0, 39)), ENT_COMPAT,'utf-8'))).'...</title>
-                    <description>'.$username.': '.htmlspecialchars(utf8_encode(htmlentities($feed['timeline'][$i]->plaintext, ENT_COMPAT,'utf-8'))).'</description>
-                    <pubDate>'.date("D, d M Y H:i:s", strtotimestamp($feed['date'][$i]->plaintext)).' +0000</pubDate>
-                    <guid>http://twitter.com'.$feed['date'][$i]->find('a', 0)->href.'</guid>
-                    <link>http://twitter.com'.$feed['date'][$i]->find('a', 0)->href.'</link>
-
-                    
-                </item>';
-
-        $rss .= '</channel></rss>';
-    
-        write_cache($username, $rss);
-    }
+        '<feed xml:lang="fr-fr" xmlns="http://www.w3.org/2005/Atom"> 
+            <title>Twitter de '.$info['username'].' / '.$info['fullname'].'</title>
+            <subtitle>'.$info['bio'].'</subtitle>
+            <link href="https://twitter.com/'.$info['username'].'"/>
+            <updated></updates>
+            <author>
+                <name>'.$info['fullname'].'</name>
+            </author>
+        ';
+    } 
 
     else $rss = read_cache($username);
     
-    header('Content-type: application/xml; charset=UTF-8');
-    echo $rss;
+    //header('Content-type: application/xml; charset=UTF-8');
+    //echo $rss;
 ?>
